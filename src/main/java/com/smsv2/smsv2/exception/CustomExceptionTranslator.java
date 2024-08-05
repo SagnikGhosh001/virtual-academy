f96d8ccd6ea121d7b78a9631b2010d2d096ab1c0
SagnikGhosh001 <sagnikghosh904@gmail.com>
2024-08-05 19:56:40 +0530

    internal server error fix4

---
+++ src/main/java/com/smsv2/smsv2/exception/CustomExceptionTranslator.java
@@ -0,0 +1,25 @@
+package com.smsv2.smsv2.exception;
+
+import org.hibernate.exception.ConstraintViolationException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.stereotype.Component;
+
+
+import java.sql.SQLException;
+
+@Component
+public class CustomExceptionTranslator {
+
+    public RuntimeException translateExceptionIfPossible(RuntimeException ex) {
+        if (ex instanceof DataIntegrityViolationException) {
+            if (ex.getCause() instanceof ConstraintViolationException) {
+                ConstraintViolationException cve = (ConstraintViolationException) ex.getCause();
+                if ("23000".equals(cve.getSQLState())) {
+                    return new ResourceInternalServerErrorException(cve.getConstraintName());
+                }
+            }
+        }
+        return ex;
+    }
+}
--- src/main/java/com/smsv2/smsv2/exception/GlobalException.java
+++ src/main/java/com/smsv2/smsv2/exception/GlobalException.java
@@ -1,6 +1,7 @@
 package com.smsv2.smsv2.exception;
 
 import java.util.Date;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -9,6 +10,10 @@ import org.springframework.web.context.request.WebRequest;
 
 @ControllerAdvice
 public class GlobalException {
+
+    @Autowired
+    private CustomExceptionTranslator exceptionTranslator;
+
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ErrorDetails> resourceNotFoundHandling(ResourceNotFoundException exception, WebRequest request) {
         ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(), request.getDescription(false));
@@ -29,7 +34,9 @@ public class GlobalException {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorDetails> globalExceptionHandling(Exception exception, WebRequest request) {
-        ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(), request.getDescription(false));
-        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+        RuntimeException translatedException = exceptionTranslator.translateExceptionIfPossible((RuntimeException) exception);
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), translatedException.getMessage(), request.getDescription(false));
+        HttpStatus status = (translatedException instanceof ResourceInternalServerErrorException) ? HttpStatus.CONFLICT : HttpStatus.INTERNAL_SERVER_ERROR;
+        return new ResponseEntity<>(errorDetails, status);
     }
 }
