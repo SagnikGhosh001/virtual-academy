6d3e12b4d49ccfe69313041b1ecc6bcf44666e98
SagnikGhosh001 <sagnikghosh904@gmail.com>
2024-08-05 19:30:38 +0530

    internal server error fix2

--- src/main/java/com/smsv2/smsv2/exception/GlobalException.java
+++ src/main/java/com/smsv2/smsv2/exception/GlobalException.java
@@ -1,7 +1,6 @@
 package com.smsv2.smsv2.exception;
 
 import java.util.Date;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,39 +9,27 @@ import org.springframework.web.context.request.WebRequest;
 
 @ControllerAdvice
 public class GlobalException {
-	// handler method to handling specific exception
-	@ExceptionHandler(ResourceNotFoundException.class)
-	public ResponseEntity<?> resourceNotFoundHandling(ResourceNotFoundException exception, WebRequest request) {
-		ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(), request.getDescription(false));
-		return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
-	}
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorDetails> resourceNotFoundHandling(ResourceNotFoundException exception, WebRequest request) {
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
 
-	// Handler method for ResourceBadRequestException
-	@ExceptionHandler(ResourceBadRequestException.class)
-	public ResponseEntity<?> resourceBadRequestHandling(ResourceBadRequestException exception, WebRequest request) {
-		ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(), request.getDescription(false));
-		return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
-	}
+    @ExceptionHandler(ResourceBadRequestException.class)
+    public ResponseEntity<ErrorDetails> resourceBadRequestHandling(ResourceBadRequestException exception, WebRequest request) {
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+    }
 
-	// Handler method for ResourceInternalServerErrorException
-//	@ExceptionHandler(ResourceInternalServerErrorException.class)
-//	public ResponseEntity<?> resourceInternalServerErrorHandling(ResourceInternalServerErrorException exception,
-//			WebRequest request) {
-//		ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(), request.getDescription(false));
-//		return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
-//	}
-	
-	 @ExceptionHandler(ResourceInternalServerErrorException.class)
-	    public ResponseEntity<ErrorDetails> handleResourceInternalServerError(
-	            ResourceInternalServerErrorException exception, WebRequest request) {
-	        ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(), request.getDescription(false));
-	        return new ResponseEntity<>(errorDetails, HttpStatus.CONFLICT);
-	    }
+    @ExceptionHandler(ResourceInternalServerErrorException.class)
+    public ResponseEntity<ErrorDetails> handleResourceInternalServerError(ResourceInternalServerErrorException exception, WebRequest request) {
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.CONFLICT);
+    }
 
-	// General handler method for other exceptions
-	@ExceptionHandler(Exception.class)
-	public ResponseEntity<?> globalExceptionHandling(Exception exception, WebRequest request) {
-		ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(), request.getDescription(false));
-		return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
-	}
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDetails> globalExceptionHandling(Exception exception, WebRequest request) {
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
--- src/main/java/com/smsv2/smsv2/exception/ResourceInternalServerErrorException.java
+++ src/main/java/com/smsv2/smsv2/exception/ResourceInternalServerErrorException.java
@@ -3,24 +3,11 @@ package com.smsv2.smsv2.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value =HttpStatus.CONFLICT)
+@ResponseStatus(value = HttpStatus.CONFLICT)
 public class ResourceInternalServerErrorException extends RuntimeException {
-
     private static final long serialVersionUID = 1L;
-
-    private String msg;
 
     public ResourceInternalServerErrorException(String msg) {
         super(String.format("'%s' Already Exist", msg));
-        this.msg = msg;
-    }
-    
-    
-    public String getMsg() {
-        return msg;
-    }
-
-    public void setMsg(String msg) {
-        this.msg = msg;
     }
 }
