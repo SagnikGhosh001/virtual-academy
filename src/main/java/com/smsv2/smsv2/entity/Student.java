764906065aaeec1772b14fdf4edbfdd27c8ef69e
SagnikGhosh001 <sagnikghosh904@gmail.com>
2024-08-09 00:38:53 +0530

    demo

--- src/main/java/com/smsv2/smsv2/entity/Student.java
+++ src/main/java/com/smsv2/smsv2/entity/Student.java
@@ -20,6 +20,7 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
